943180f51c5decf3f3c495704f6f3f406d9205d9
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-01-27 16:25:01 +0600

    Added input validation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-document.getElementById('login-btn').addEventListener('click', 
-function(){
+// Login button event handler
+document.getElementById('login-btn').addEventListener('click', function(){
     document.getElementById('login-area').style.display = "none";
     document.getElementById('transaction-area').style.display = "block";
 });
- 
+// Deposit button event handler
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const newAmount = parseFloat(document.getElementById('deposit-amount').value);
     // Add amount to deposit
@@ -11,9 +11,10 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     // Add amount to balance
     addAmount('balance',newAmount);
     document.getElementById('deposit-amount').value = '';
+    validateForm('deposit-amount');
 });
 
-
+// Withdraw button event handler
 document.getElementById('withdraw-btn').addEventListener('click', function(){
     const newAmount = parseFloat(document.getElementById('withdraw-amount').value);
     // Add amount to withdraw
@@ -21,12 +22,44 @@ document.getElementById('withdraw-btn').addEventListener('click', function(){
     document.getElementById('withdraw-amount').value = '';
 
     const oldBalance = parseFloat(document.getElementById('balance').innerText);
-    document.getElementById('balance').innerText = oldBalance - newAmount;
+    if(isNaN(newAmount)){
+        document.getElementById('balance').innerText = oldBalance;
+        alert('Enter valid amount');
+    }
+    else{
+        document.getElementById('balance').innerText = oldBalance - newAmount;
+    }
+
+    if(newAmount > oldBalance || newAmount <= 0){
+        document.getElementById('balance').innerText = oldBalance;
+        alert('Enter valid amount');
+    }
+    // document.getElementById('balance').innerText = oldBalance - newAmount;
 })
 function addAmount(id, newAmount) {
     const oldAmount = parseFloat(document.getElementById(id).innerText);
     document.getElementById(id).innerText = oldAmount + newAmount;
+
+    if(isNaN(newAmount) || newAmount <= 0){
+        document.getElementById(id).innerText = oldAmount;
+        alert('Enter valid amount');
+    }
+    else{
+        document.getElementById(id).innerText = oldAmount + newAmount;
+    }
 };
-function getInputNumber() {
-    
-} 
+
+// const inputAmount = document.getElementById('withdraw-amount').value;
+// if(inputAmount == ""){
+//     alert('Enter valid amount');
+// }
+// function validateForm(amount) {
+//     const value = document.getElementById(amount).value;
+//     if (isNaN(value) || value == 0) {
+//     //   alert("Enter valid amount");
+
+//     }
+//     else{
+//         document.getElementById('balance').innerText = 500;
+//     }
+// }
